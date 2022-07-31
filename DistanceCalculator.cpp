e764f64fb614239cda185e0e617ee553106f5de3
Sagie Tsentsiper <tsents.sagie@gmail.com>
2022-07-31 15:25:18 +0300

    fix to distance calculator

--- DistanceCalculator.cpp
+++ DistanceCalculator.cpp
@@ -1,5 +1,7 @@
 #include "DistanceCalculator.h"
 
+#include <cmath>
+
 double DistanceCalculator::euclidean(const Flower &f1, const Flower &f2) {
     double sum = 0;
     sum += pow(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength(), 2);
@@ -12,19 +14,19 @@ double DistanceCalculator::euclidean(const Flower &f1, const Flower &f2) {
 double DistanceCalculator::chebyshev(const Flower &f1, const Flower &f2) {
     //the distance is the max of the simple distances
     double max = 0;
-    max = std::max(abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength()), max);
-    max = std::max(abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth()), max);
-    max = std::max(abs(f1.getPetalLength() - f2.getPetalLength()), max);
-    max = std::max(abs(f1.getPetalWidth() - f2.getPetalWidth()), max);
+    max = std::max(std::abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength()), max);
+    max = std::max(std::abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth()), max);
+    max = std::max(std::abs(f1.getPetalLength() - f2.getPetalLength()), max);
+    max = std::max(std::abs(f1.getPetalWidth() - f2.getPetalWidth()), max);
     return max;
 }
 
 double DistanceCalculator::manhattan(const Flower &f1, const Flower &f2) {
     //https://iq.opengenus.org/manhattan-distance/#:~:text=Manhattan%20distance%20is%20a%20distance,all%20dimensions%20of%20two%20points.
     double distance = 0;
-    distance += std::abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength());
-    distance += std::abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth());
-    distance += std::abs(f1.getPetalLength() - f2.getPetalLength());
-    distance += std::abs(f1.getPetalWidth() - f2.getPetalWidth());
+    distance += abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength());
+    distance += abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth());
+    distance += abs(f1.getPetalLength() - f2.getPetalLength());
+    distance += abs(f1.getPetalWidth() - f2.getPetalWidth());
     return distance;
 }
