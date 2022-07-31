613034977e720b2fc6efe9cc6c125a597ed45731
Sagie Tsentsiper <tsents.sagie@gmail.com>
2022-07-31 15:59:53 +0300

    fix to distance calculator, all sorts work!

--- DistanceCalculator.cpp
+++ DistanceCalculator.cpp
@@ -24,9 +24,9 @@ double DistanceCalculator::chebyshev(const Flower &f1, const Flower &f2) {
 double DistanceCalculator::manhattan(const Flower &f1, const Flower &f2) {
     //https://iq.opengenus.org/manhattan-distance/#:~:text=Manhattan%20distance%20is%20a%20distance,all%20dimensions%20of%20two%20points.
     double distance = 0;
-    distance += abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength());
-    distance += abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth());
-    distance += abs(f1.getPetalLength() - f2.getPetalLength());
-    distance += abs(f1.getPetalWidth() - f2.getPetalWidth());
+    distance += std::abs(f1.getCalyxLeavesLength() - f2.getCalyxLeavesLength());
+    distance += std::abs(f1.getCalyxLeavesWidth() - f2.getCalyxLeavesWidth());
+    distance += std::abs(f1.getPetalLength() - f2.getPetalLength());
+    distance += std::abs(f1.getPetalWidth() - f2.getPetalWidth());
     return distance;
 }
